3e87f22805bf38d99548f2b9660e3e55a7e32df2
Miguel67mdq <miguel_angeldominguez@hotmail.com>
2023-03-01 20:17:26 -0300

    Se entrega tarea Clase5Js

--- Clase-5-Js/tareas.js
+++ Clase-5-Js/tareas.js
@@ -7,7 +7,20 @@ function counter() {
   // ejemplo: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+  //let counter = 1;
+   //return () => counter++;
+   let  counter = 0;
+    return function() {
+      return  ++counter;
+    } 
 }
+const newCounter = counter();
+console.log(newCounter()); // 1
+console.log(newCounter()); // 2
+console.log(newCounter()); // 3
+
+
+
 
 function cacheFunction(cb) {
   // Usa closures para crear un caché para la función cb.
@@ -21,7 +34,26 @@ function cacheFunction(cb) {
   // si la invocas de nuevo con 5, deberia retornar 25 (guardado previament en el cache)
   // Tips, usá un objeto donde cada propiedad sea un argumento, y el valor el resultado.
   // usá hasOwnProperty!
-}
+  let cache = {};
+    return function (argumento){
+      if (cache.hasOwnProperty(argumento)){
+        return cache[argumento];
+      }else{
+        cache[argumento] = cb(argumento);
+        return cache[argumento];
+      }
+    
+    }
+    
+  }
+
+  const cachefun = cacheFunction(function(x) { return x * x; })
+
+  console.log(cachefun(5))
+  console.log(cachefun(4))
+  console.log(cachefun(25))
+  console.log(cachefun(5))
+
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
